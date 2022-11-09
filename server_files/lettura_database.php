5bb4e3944594f34ce54613df7c6fb0421d791cb6
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2022-11-09 19:33:37 +0100

    Update lettura_database.php

--- server_files/lettura_database.php
+++ server_files/lettura_database.php
@@ -7,7 +7,7 @@
     
     $classe = $_POST['classe'];
     $hostname="localhost";
-	$link = mysqli_connect($hostname,$username, $password, $dbname) or die ("html>script language='JavaScript'>alert('Unable to connect to database! Please try again later.'),history.go(-1)/script>/html>");
+    $link = mysqli_connect($hostname,$username, $password, $dbname) or die ("html>script language='JavaScript'>alert('Unable to connect to database! Please try again later.'),history.go(-1)/script>/html>");
 	
     $query = "SELECT * FROM studenti WHERE classe='$classe'";
 	$result = mysqli_query($link, $query);
